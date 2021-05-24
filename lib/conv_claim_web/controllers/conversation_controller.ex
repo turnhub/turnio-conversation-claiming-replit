032b1f721b5514b4871a7a5e340f6645a01d6517
Simon de Haan <simon@praekelt.org>
2021-05-24 13:43:03 +0200

    add debugging

--- lib/conv_claim_web/controllers/conversation_controller.ex
+++ lib/conv_claim_web/controllers/conversation_controller.ex
@@ -1,18 +1,35 @@
 defmodule ConvClaimWeb.ConversationController do
   use ConvClaimWeb, :controller
   alias ConvClaim.TurnClient
-  action_fallback ConvClaimWeb.FallbackController
+  action_fallback(ConvClaimWeb.FallbackController)
 
   def create(conn, %{"to" => to, "type" => "text", "text" => %{"body" => body}}) do
-    reply_text = "You said: #{inspect(body)}"
+    reply_text = "You said: #{inspect(body)}" |> IO.inspect(label: "reply text")
     client = TurnClient.client()
 
-    with [claim] <- Plug.Conn.get_req_header(conn, "x-turn-claim"),
-         {:ok, _resp} <-
-           TurnClient.send_text_message(client, to, reply_text, [{"x-turn-claim-release", claim}]) do
+    with [claim] <-
+           Plug.Conn.get_req_header(conn, "x-turn-claim") |> IO.inspect(label: "claim header?"),
+         {:ok, %Tesla.Env{status: status}} when status in [200, 201] <-
+           TurnClient.send_text_message(client, to, reply_text, [{"x-turn-claim-release", claim}])
+           |> IO.inspect(label: "turn response") do
       conn
       |> put_status(:created)
       |> Plug.Conn.send_resp(201, "ok")
+    else
+      {:ok, %Tesla.Env{status: status}} ->
+        conn
+        |> put_status(status)
+        |> Plug.Conn.send_resp(status, "Turn response code")
+
+      {:error, %Tesla.Env{status: status}} ->
+        conn
+        |> put_status(status)
+        |> Plug.Conn.send_resp(status, "Turn response code")
+
+      {:error, reason} ->
+        conn
+        |> put_status(503)
+        |> Plug.Conn.send_resp(503, "Turn error: #{inspect(reason)}")
     end
   end
 end
